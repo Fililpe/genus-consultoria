6386fa7e6b627fa85de2273a2013c3f828bb47a6
Filipe Sousa <90399125+Fililpe@users.noreply.github.com>
2023-12-27 20:54:21 -0300

    fix bugs, add styles cookies

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-
 window.addEventListener("scroll", () => {
   const nav = document.querySelector(".nav")
-    
-  
-    if (window.scrollY > 100) {
-      nav.classList.add("nav-fixed");
-    } else {
-      nav.classList.remove("nav-fixed");
-    }
-  });
 
-  var msgCookies = document.getElementById('cookies-msg')
+  if (window.scrollY > 100) {
+    nav.classList.add("nav-fixed")
+  } else {
+    nav.classList.remove("nav-fixed")
+  }
+})
 
-function aceito(){
+var msgCookies = document.getElementById("cookies-msg")
+
+function aceito() {
   localStorage.lgpd = "sim"
-  msgCookies.classList.remove('mostrar')
+  msgCookies.style.display = "none"
 }
 
-if(localStorage.lgpd == 'sim'){
-  msgCookies.classList.remove('mostrar')
-} else{
-  msgCookies.classList.add('mostrar')
+if (localStorage.lgpd == "sim") {
+  msgCookies.style.display = "none"
+} else {
+  msgCookies.style.display = "block"
 }
-
