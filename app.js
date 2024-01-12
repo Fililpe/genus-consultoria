933db140510c97305cf3e2a717d92328ee6f0cfa
Filipe Sousa <90399125+Fililpe@users.noreply.github.com>
2024-01-11 21:15:50 -0300

    grande mudança
    nova secção +
    secção "soluções" totalmente repaginada +
    fix bugs +

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 window.addEventListener("scroll", () => {
   const nav = document.querySelector(".nav")
-
-  if (window.scrollY > 100) {
-    nav.classList.add("nav-fixed")
-  } else {
-    nav.classList.remove("nav-fixed")
-  }
-})
+    
+    if (window.scrollY > 100) {
+      nav.classList.add("nav-fixed");
+    } else {
+      nav.classList.remove("nav-fixed");
+    }
+  });
 
 var msgCookies = document.getElementById("cookies-msg")
 
