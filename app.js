d1d5e120e1a39241ac6b10a03af5eb672cdcfc90
Filipe Sousa <90399125+Fililpe@users.noreply.github.com>
2024-01-31 19:37:16 -0300

    Update JavaScript and CSS code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-  // toggle menu
+
 window.addEventListener("scroll", () => {
   let nav = document.querySelector(".nav")
     
@@ -8,67 +8,36 @@ window.addEventListener("scroll", () => {
       nav.classList.remove("nav-fixed");
     }
   });
-  // toggle menu
-
-
-//
+// toggle menu
 
 //carrossel
 
-// const box = document.querySelector(".carrossel-container")
-// const carrosselImagens = document.querySelectorAll (".carrossel-container img")
-
-// let contadorCarrosel = 0
-
-// function slider() {
-//   contadorCarrosel++
-
-//   if (contadorCarrosel > carrosselImagens.length - 1) {
-//     contadorCarrosel = 0
-//   }
-
-//   box.style.transform = `translateX(${-contadorCarrosel * 1250}px)`
-  
-// }
-
-// setInterval(slider, 5000 ) //ms
-
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
-  const carrosselContainer = document.querySelector(".carrossel-container");
+  const carrosselContainer = document.querySelector(".carrossel-container")
   let currentIndex = 0;
 
   function showImage(index) {
     const newPosition = -index * 100 + "%";
-    carrosselContainer.style.transform = "translateX(" + newPosition + ")";
+    carrosselContainer.style.transform = "translateX(" + newPosition + ")"
   }
 
   function nextImage() {
-    currentIndex = (currentIndex + 1) % carrosselContainer.children.length;
-    showImage(currentIndex);
+    currentIndex = (currentIndex + 1) % carrosselContainer.children.length
+    showImage(currentIndex)
   }
 
   function prevImage() {
     currentIndex =
       (currentIndex - 1 + carrosselContainer.children.length) %
       carrosselContainer.children.length;
-    showImage(currentIndex);
+    showImage(currentIndex)
   }
 
-  // Intervalo para trocar as imagens automaticamente (opcional)
-  setInterval(nextImage, 5000); // Muda a cada 5 segundos, ajuste conforme necessário
+  setInterval(nextImage, 5000)
 });
 
 
-
-//carrossel
-
-
-  
-
-  // cookies
+// cookies
 var msgCookies = document.getElementById("cookies-msg")
 
 function aceito() {
@@ -81,4 +50,3 @@ if (localStorage.lgpd == "sim") {
 } else {
   msgCookies.style.display = "block"
 }
-  // cookies
